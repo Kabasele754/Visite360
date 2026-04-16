f031184d7472979d0adc16e49b16616de12312c9
Achille kabasele <pepexykabasele@gmail.com>
2026-04-16 20:10:52 +0200

    fix static

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,9 +76,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
