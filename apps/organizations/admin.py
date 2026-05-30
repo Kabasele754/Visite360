28d5a8ca4577a08bfebd09c19c04c2b5da6144ab
Perce Madjunda <percemadjunda23@gmail.com>
2026-05-30 04:26:11 +0200

    add models to admin django

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from apps.organizations.models import Organization
+from apps.places.models import Place
+
 # Register your models here.
+
+
+
+admin.site.register(Organization)
+admin.site.register(Place)
+
+
+
--- apps/tours/admin.py
+++ apps/tours/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from apps.tours.models import Scene360
+from apps.tours.models import Hotspot, Scene360, Tour
 
 # Register your models here.
 
 
 admin.site.register(Scene360)
+
+admin.site.register(Tour)
+admin.site.register(Scene360)
+admin.site.register(Hotspot)
 
